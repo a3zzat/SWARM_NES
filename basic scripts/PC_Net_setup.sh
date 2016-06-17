a68883eb22cd6c3a4277e0c0edfda6ab9d2e3e71
a3zzat <viva.amego@gmail.com>
2016-06-17 09:08:50 -0500

    update scripts 2

---
+++ basic scripts/PC_Net_setup.sh
@@ -0,0 +1,4 @@
+ping 192.168.1.130 -c 8
+echo export ROS_MASTER_URI=http://192.168.1.130:11311 >> ~/.bashrc
+# please change with your ip address
+echo export ROS_HOSTNAME=192.168.1.135 >> ~/.bashrc
---
+++ basic scripts/Turtlebot_Net_setup.sh
@@ -0,0 +1,3 @@
+ping 192.168.1.130 -c 8
+echo export ROS_MASTER_URI=http://192.168.1.130:11311 >> ~/.bashrc
+echo export ROS_HOSTNAME=192.168.1.130 >> ~/.bashrc
